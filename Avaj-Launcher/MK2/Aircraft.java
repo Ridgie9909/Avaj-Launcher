1bafd1a8bcb55458507ba0282e7333c81516532f
Ridge DUBE <rdube@c3r4s4.wethinkcode.co.za>
2019-07-01 11:41:27 +0200

    1st July 11:41

---
+++ Avaj-Launcher/MK2/Aircraft.java
@@ -0,0 +1,15 @@
+public class Aircraft
+{
+    public static void main(void)
+    {
+        long id = String uniqueID = UUID.randomUUID().toString();
+        String name;
+        Coordinates coordinates;
+        try{
+
+        }
+        catch(){
+            
+        }
+    }
+}
---
+++ Avaj-Launcher/MK2/Tower.java
@@ -0,0 +1,28 @@
+public class Tower
+{
+    char Flyable = null;
+    i = 0;
+    public stataic void main(string[] info)
+    {
+        char observers
+        {
+            if(Flyable != null)
+            {
+                void register()
+                {
+
+                }
+            }
+            else
+            {
+                void unregister()
+                {
+                
+                }
+            }
+
+            return (Flyable)
+
+        }
+    }
+}
