51728ecf07b24e6e304c6e4f0fc1bd445a4db9ee
Ridge <rdube@student.wethinkcode.co.za>
2020-01-19 08:22:29 +0200

    git update

--- Avaj_Launcher/avaj_launcher/aircraft/Flyable.java
+++
@@ -1,8 +0,0 @@
-package avaj_launcher.aircraft;
-
-public interface Flyable
-{
-    public void updateconditions();
-    public void registerTower(WeatherTower weatherTower);
-    public Coordinates getCoordinates();
-}
